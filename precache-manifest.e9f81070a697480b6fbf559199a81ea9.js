535659ed2e3a8a89450b64ceba8da84c31b0ee87
Carlos Melgoza <carlos.melgoza@qacg.com>
2020-04-14 17:27:50 -0500

    Updates

---
+++ precache-manifest.e9f81070a697480b6fbf559199a81ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59013a4ef0f6ea119b45f87376d1c50",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "48d76ceea4584cd724d0",
+    "url": "/monsters-rolodex/static/css/main.fcd6d51e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48d76ceea4584cd724d0",
+    "url": "/monsters-rolodex/static/js/main.a02dd1d2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
